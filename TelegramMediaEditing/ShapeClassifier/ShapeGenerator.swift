f199566f52077080ba5a27829bd2e37e3f8e615e
Alex Gra <sharrikk@gmail.com>
2022-10-31 21:59:03 +0300

    - better star generation

--- TelegramMediaEditing/ShapeClassifier/ShapeGenerator.swift
+++ TelegramMediaEditing/ShapeClassifier/ShapeGenerator.swift
@@ -17,19 +17,20 @@ extension ShapeClassifier.Shape {
             return UIBezierPath(rect: CGRect(mid: center, size: size))
             
         case .star(center: let center, size: let size):
-            let path = UIBezierPath()
-            let r = size / 2.0
-            let flip: CGFloat = -1.0 // use this to flip the figure 1.0 or -1.0
-            let polySide = CGFloat(5)
-            let theta = 2.0 * Double.pi * Double(2.0 / polySide)
-            path.move(to: CGPoint(x: center.x, y: r * flip + center.y))
-            for i in 1..<Int(polySide) {
-                let x: CGFloat = r * CGFloat( sin(Double(i) * theta) )
-                let y: CGFloat = r * CGFloat( cos(Double(i) * theta) )
-                path.addLine(to: CGPoint(x: x + center.x, y: y * flip + center.y))
-            }
-            path.close()
-            return path
+            return generateStar(center: center, size: size)
+//            let path = UIBezierPath()
+//            let r = size / 2.0
+//            let flip: CGFloat = -1.0 // use this to flip the figure 1.0 or -1.0
+//            let polySide = CGFloat(5)
+//            let theta = 2.0 * Double.pi * Double(2.0 / polySide)
+//            path.move(to: CGPoint(x: center.x, y: r * flip + center.y))
+//            for i in 1..<Int(polySide) {
+//                let x: CGFloat = r * CGFloat( sin(Double(i) * theta) )
+//                let y: CGFloat = r * CGFloat( cos(Double(i) * theta) )
+//                path.addLine(to: CGPoint(x: x + center.x, y: y * flip + center.y))
+//            }
+//            path.close()
+//            return path
             
         case .rhombus(center: let center, size: let size):
             let path = UIBezierPath()
@@ -58,4 +59,23 @@ extension ShapeClassifier.Shape {
         return nil
     }
     
+    fileprivate func generateStar(center: CGPoint, size: CGFloat) -> UIBezierPath {
+        // better star
+        let pointsTuples: [CGPoint] = [CGPoint(x: 50, y: 4.55270614), CGPoint(x: 61.2240581, y: 37.5514093), CGPoint(x: 96.0761145, y: 38.0289629), CGPoint(x: 68.1609076, y: 58.9008366), CGPoint(x: 78.4766048, y: 92.1946841), CGPoint(x: 50, y: 72.0955083), CGPoint(x: 21.5233952, y: 92.1946841), CGPoint(x: 31.8390924, y: 58.9008366), CGPoint(x: 3.92388548, y: 38.0289629), CGPoint(x: 38.7759419, y: 37.5514093), CGPoint(x: 50, y: 4.55270614)]
+        
+        let points = pointsTuples.map { (p: CGPoint)->CGPoint in
+            CGPoint(x: (p.x-50)*size/100 + center.x,
+                    y: (p.y-50)*size/100 + center.y)
+        }
+        let bezier = UIBezierPath()
+        for (i, p) in points.enumerated() {
+            if i == 0 {
+                bezier.move(to: p)
+            } else {
+                bezier.addLine(to: p)
+            }
+        }
+        bezier.close()
+        return bezier
+    }
 }
